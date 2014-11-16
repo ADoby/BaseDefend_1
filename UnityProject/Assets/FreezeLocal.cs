9b8100f6193a757d850d62b84ea23cdae6daecab
ADoby <zimmerlint@gmail.com>
2014-11-16 23:58:59 +0100

    Update 0.4
    First Enemie with Ragdoll, how to animate ?

---
+++ UnityProject/Assets/FreezeLocal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeLocal : MonoBehaviour {
+
+    public bool Position;
+    public bool Rotation;
+
+    private Vector3 position;
+    private Quaternion rotation;
+
+	// Use this for initialization
+	void Start () 
+    {
+        position = transform.localPosition;
+        rotation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	}
+
+    void FixedUpdate()
+    {
+    }
+
+    void LateUpdate()
+    {
+        if (Position) transform.localPosition = position;
+        if (Rotation) transform.localRotation = rotation;
+    }
+}
