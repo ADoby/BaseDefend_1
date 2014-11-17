e61727d5dd42a8bbe099e6de4ba614d61791b347
ADoby <zimmerlint@gmail.com>
2014-11-17 09:19:20 +0100

    Update 0.4.1
    Nothing

---
+++ UnityProject/Assets/LegMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LegMovement : MonoBehaviour {
+
+    public float startTime = 0.5f;
+
+    
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+
+    void Update()
+    {
+
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+	    
+	}
+}
